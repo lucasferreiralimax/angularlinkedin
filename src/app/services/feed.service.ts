d7dc7930805ec06a9eb50fb5518b02f8639f986d
Lucas Lima <lucaslimay@gmail.com>
2018-09-25 23:47:15 -0300

    adicionando foto de publicidade e ajuste de tamanho do feed

--- src/app/services/feed.service.ts
+++ src/app/services/feed.service.ts
@@ -26,6 +26,24 @@ export class FeedService {
       "content": "Valorize quem constrói, pois quem destrói não estuda e para atrasar tem muitos!",
       "likes": 999000666,
       "photo": "https://media.licdn.com/dms/image/C4E0BAQFgxNeOUH5CZA/company-logo_200_200/0?e=1545868800&v=beta&t=2roycseuANXoEMP5AHPfF0eOoNmadPCrHw5wWOvVv2g"
+    },
+    {
+      "name": "Bill Gates",
+      "content": "Como eu amo os brasileiros, sem eles acho que não teriamos nada de evolução!",
+      "likes": 451231,
+      "photo": "https://media.licdn.com/dms/image/C5603AQHv9IK9Ts0dFA/profile-displayphoto-shrink_800_800/0?e=1543449600&v=beta&t=1rOBA_CflNKl3nlcLUx_Eblu1kH0MSOfwk6F8399b-E"
+    }
+    {
+      "name": "Reid Hoffman",
+      "content": "O Linkedin precisa entender o melhor ajudar e não censurar os desenvolvedores que estão construindo um futuro melhor.",
+      "likes": 6,
+      "photo": "https://media.licdn.com/dms/image/C5603AQGBG-pWCdQcIA/profile-displayphoto-shrink_800_800/0?e=1543449600&v=beta&t=vV4r2CKvtMHixxtT3WyQ7BTq_ZodP6aIvbOwXuM68FM"
+    },
+    {
+      "name": "Mark Zuckerberg",
+      "content": "Tenho que admitir, os brasileiros são brilhantes!",
+      "likes": 999666,
+      "photo": "https://media.licdn.com/dms/image/C4E0BAQFgxNeOUH5CZA/company-logo_200_200/0?e=1545868800&v=beta&t=2roycseuANXoEMP5AHPfF0eOoNmadPCrHw5wWOvVv2g"
     }
   ]
 
