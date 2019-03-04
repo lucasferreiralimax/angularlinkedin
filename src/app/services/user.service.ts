d2fac8915e2a56b8e317f03c9c24c9330c125620
Lucas Lima <lucaslimay@gmail.com>
2019-03-04 19:35:23 -0300

    upgrade image

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -11,6 +11,6 @@ export class UserService {
     "name": "Lucas Lima",
     "job": "Front-end at Open Source",
     "connections": 99,
-    "photo": "https://media.licdn.com/dms/image/C4D03AQEjKZ0BTcUe-A/profile-displayphoto-shrink_200_200/0?e=1545264000&v=beta&t=umXKWez5jxfuTWnNMPjTvPd4-9SUYy8fBcx3YVUzWIw"
+    "photo": "./assets/lucas.jpeg"
   }
 }
